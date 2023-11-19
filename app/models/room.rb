43ffb6ae327b29f1ca4b3ac72636dbf03be830a4
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-11-19 22:21:44 +0900

    Memberモデル作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
 
   validates :cw_room_name, presence: true
   validates :cw_room_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_19_131057) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_131835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.string "cw_user_name", null: false
+    t.integer "cw_account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_members_on_room_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -34,5 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_19_131057) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "members", "rooms"
   add_foreign_key "rooms", "users"
 end
