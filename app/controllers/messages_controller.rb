8c09e98045316b07571168125192990dc22f212d
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-11-21 22:26:32 +0900

    メンバーテーブルを削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,19 +22,6 @@ class MessagesController < ApplicationController
         cw_room_name: room_data["name"],
         cw_room_id: room_data["room_id"]
       )
-      members_url = URI("https://api.chatwork.com/v2/rooms/#{room_data["room_id"]}/members")
-      members_request = Net::HTTP::Get.new(members_url)
-      members_request["accept"] = 'application/json'
-      members_request["x-chatworktoken"] = current_user.user_token
-      members_response = http.request(members_request)
-      members_data = JSON.parse(members_response.body)
-      members_data.each do |member_data|
-        Member.create(
-          room_id: room.id,
-          cw_user_name: member_data["name"],
-          cw_account_id: member_data["account_id"]
-        )
-      end
     end
     redirect_to messages_path
   end
